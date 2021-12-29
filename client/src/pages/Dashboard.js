817f55d5bb4b878935fbbffb31ddb048a80fef13
Sajal Gupta <guptasajal411@gmail.com>
2021-12-29 21:31:36 +0530

    refactor: :rocket: Optimize by removing unused useState

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,37 +1,33 @@
 import React, { useEffect, useState } from 'react'
 
 export default  function Dashboard() {
-    // const [verifyResult, setVerifyResult] = useState();
     const [errorMessage, setErrorMessage] = useState();
     const [username, setUsername] = useState();
     const [dashboardData, setDashboardData] = useState([]);
     useEffect(() => {
-        async function verify(){
+        async function dashboard(){
             if (localStorage.getItem("token")){
             await fetch("http://localhost:3001/api/dashboard", {
                     headers: { "x-access-token": localStorage.getItem("token") },
-                    method: "POST"
+                    method: "GET"
                 })
                 .then(response => response.json())
                 .then((jsonData) => {
                     console.log(jsonData);
                     if (jsonData.status === "ok"){
-                        // setVerifyResult(true);
                         setUsername(jsonData.username);
                         setDashboardData(jsonData.dashboardData);
                         console.log(jsonData);
                     } else {
-                        // setVerifyResult(false)
                         setErrorMessage(jsonData.message);
                     }
                 });
             } else {
                 // token not available
-                // setVerifyResult(false);
                 setErrorMessage("JWT not available. Please try logging in first.");
             }
         }
-        verify();
+        dashboard();
     }, []);
     return (
         <div>
