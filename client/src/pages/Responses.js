a1631bb2631bc4fc954163fecbfcd4f60de06892
Sajal Gupta <guptasajal411@gmail.com>
2022-02-03 17:02:56 +0530

    feat: :rocket: Create UI for Responses page

--- client/src/pages/Responses.js
+++ client/src/pages/Responses.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import domain from "../common/api";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./css/Responses.css";
+import "./css/Fonts.css";
+
 
 export default function Responses() {
+    const navigate = useNavigate();
     const params = useParams();
     const [errorMessage, setErrorMessage] = useState();
     const [formTitle, setFormTitle] = useState();
@@ -43,51 +48,72 @@ export default function Responses() {
 
     return (
         <div>
-            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-            {formTitle && formDescription && formViews &&
-                <form>
-                    <h1>{formTitle}</h1>
-                    <p>{formDescription}</p>
-                    <p>Form views: {formViews}</p>
+            <div className="section1 d-flex align-items-center justify-content-center">
+                <div className="responsesPage p-4 shadow-lg">
+                    {errorMessage &&
+                        <div className="d-flex align-items-center justify-content-center flex-column mt-4">
+                            <h5 className="text-center mb-4" style={{ color: "#ed4245" }}>{errorMessage}</h5>
+                            <button onClick={() => { navigate("/dashboard") }} className="errorButton white py-2 px-3">Return to your Dashboard</button>
+                        </div>
+                    }
+                    {formTitle && formDescription && formViews && !errorMessage &&
+                        <div>
+                            <h3 className="text-start white mt-2">{formTitle}</h3>
+                            <p className="text-start white2 mt-3">{formDescription}</p>
+                            <hr className="formRule" />
+                        </div>
+                    }
                     {formQuestions.map((question, index) => (
-                        <div key={`${question}-${index}`} style={{ border: "1px solid red", marginBottom: "10px", width: "fit-content", padding: "20px 10px" }}>
-                            <h3 style={{ margin: "0" }}>{index + 1}. {question.question}</h3>
-                            <br />
-                            <h4 style={{ margin: "0" }}>{question.description}</h4>
-                            <br />
+                        <div key={`${question}-${index}`} className="formQuestion my-4">
+                            <h5 className="question mb-2 white">{question.question}</h5>
+                            <p className="responseLabel white2 m-0">{question.description}</p>
                             {question.type === "text" &&
-                                <div>
+                                <div className="white responsesBox px-3 my-2 py-2">
                                     {question.answers.map((singleAnswer, singleAnswerIndex) => (
                                         <div>
-                                            <p>{singleAnswerIndex + 1}. {singleAnswer}</p>
+                                            <p className="m-0 textResponse">{singleAnswerIndex + 1}. {singleAnswer}</p>
                                         </div>
                                     ))}
                                 </div>
                             }
-                            {question.type !== "text" &&
-                                <div>
-                                    <table>
-                                        <tr>
-                                            <td>Sr.</td>
-                                            {question.options.map((option) => (
-                                                <td>{option}</td>
-                                            ))}
-                                        </tr>
-                                    </table>
-                                    {question.answers.map((singleAnswer, singleAnswerIndex) => (
-                                        <tr>
-                                            <td>{singleAnswerIndex + 1}. </td>
-                                            {singleAnswer.map((singleAnswerBool) => (
-                                                <td>{singleAnswerBool ? <p>✅</p> : <p>❎</p>}</td>
-                                            ))}
-                                        </tr>
-                                    ))}
+                            {question.type === "scq" &&
+                                <div className="white responsesBox px-3 my-2 py-2">
+                                    {
+                                        question.answers.map((singleAnswerArray, singleAnswerIndexArray) => (
+                                            <div>
+                                                {singleAnswerArray.map((singleAnswer, singleAnswerIndex) => (
+                                                    <div>
+                                                        {singleAnswer === true && <p className="m-0">{singleAnswerIndexArray + 1}. {question.options[singleAnswerIndex]}</p>}
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            }
+                            {question.type === "mcq" &&
+                                <div className="white responsesBox px-3 my-2 py-2">
+                                    {
+                                        question.answers.map((singleAnswerArray, singleAnswerIndexArray) => (
+                                            <div>
+                                                <p className="m-0">{singleAnswerIndexArray + 1}.&nbsp;
+                                                    {singleAnswerArray.map((singleAnswer, singleAnswerIndex) => (
+                                                        <>
+                                                            {singleAnswer === true && <>{question.options[singleAnswerIndex]} | </>}
+                                                            {/* {singleAnswer === true && singleAnswerIndex !== singleAnswerArray.length - 1 && <>{question.options[singleAnswerIndex]}</> } */}
+                                                            {/* {singleAnswer === true && singleAnswerIndex === singleAnswerArray.length - 1 && <>{question.options[singleAnswerIndex]}</> } */}
+                                                        </>
+                                                    ))}
+                                                </p>
+                                            </div>
+                                        ))
+                                    }
                                 </div>
                             }
                         </div>
                     ))}
-                </form>
-            }
+                </div>
+            </div>
         </div>
     )
 }
